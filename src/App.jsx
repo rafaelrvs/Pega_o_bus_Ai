456b6fcd3e21144a1a222c7b0ca76e6a4b02b31a
rafaelrvs <rafaelvieira1333@gmail.com>
2024-10-11 20:59:33 -0300

    Adicionando melhoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header/header'
+import Header from './components/header/header.jsx'
 import { GlobalStorage } from './components/Context/Globalcontext/Globalcontext'
-import Main from './components/main/main'
+import Main from './components/main/Main.jsx'
 
 function App() {
 
